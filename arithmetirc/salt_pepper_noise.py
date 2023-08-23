c939d42fa4fe342d64fd6441f299074e793c07a1
yannan1 <yanaenen94@163.com>
2023-08-23 15:26:27 +0800

    add salt pepper noise

---
+++ arithmetirc/salt_pepper_noise.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+"""
+author：yannan1
+since：2023-08-22
+"""
+import random
+
+import torch
+from imgaug import augmenters as iaa
+
+
+def imgaug_example(image, p=0.03):
+    """
+    args:
+        image: np.ndarray
+        p: 将像素替换为椒盐噪声的概率。 取值范围(0.0, 0.03)
+    """
+    augmenter = iaa.SaltAndPepper(p=p)
+    image = augmenter(image=image)
+    return image
+
+
+def torch_example(image, p=0.03):
+    """
+    args:
+        image: torch.Tensor
+        p: 将像素替换为椒盐噪声的概率。 取值范围(0.0, 0.03)
+    """
+
+    _, imh, imw = image.shape
+    num_salt = int(p * imh * imw)
+
+    num_salt_0 = random.randint(0, num_salt)
+    num_salt_255 = num_salt - num_salt_0
+
+    coords1 = [torch.randint(low=0, high=i - 1, size=(num_salt_0,)) for i in [imh, imw]]
+    image[:, coords1[0], coords1[1]] = 0
+
+    coords2 = [torch.randint(low=0, high=i - 1, size=(num_salt_255,)) for i in [imh, imw]]
+    image[:, coords2[0], coords2[1]] = 255
+
+    return image
+
+
+
+
+
+
---
+++ test.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+"""
+author：yannan1
+since：2023-08-22
+"""
+import cv2
+import torch
+import matplotlib.pyplot as plt
+
+from arithmetirc.salt_pepper_noise import imgaug_example, torch_example
+
+if __name__ == '__main__':
+    img_path = '../data/dog-cat.jpg'
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    torch_img = torch.from_numpy(img).to(device)
+    torch_img = torch_img.permute((2, 0, 1)).float()
+
+    imgaug_out = imgaug_example(img)
+    torch_out = torch_example(torch_img)
+
+    torch_out = torch_out.permute((1, 2, 0)).cpu().numpy().astype('uint8')
+
+    cv2.imwrite('salt_pepper_noise-imgaug.jpg', cv2.cvtColor(imgaug_out, cv2.COLOR_RGB2BGR))
+    cv2.imwrite('salt_pepper_noise-torch.jpg', cv2.cvtColor(torch_out, cv2.COLOR_RGB2BGR))
+
+    # fig, axes = plt.subplots(1, 3)
+    # axes[0].imshow(img)
+    # axes[0].axis('off')
+    # # axes[0].set_title('原图')
+    #
+    # axes[1].imshow(imgaug_out)
+    # axes[1].axis('off')
+    # axes[1].text(0.5, -0.15, 'imgaug', transform=axes[1].transAxes,
+    #              fontsize=12, ha='center')
+    #
+    # axes[2].imshow(torch_out)
+    # axes[2].axis('off')
+    # axes[1].text(0.5, -0.15, 'torch', transform=axes[2].transAxes,
+    #              fontsize=12, ha='center')
+    #
+    # plt.subplots_adjust(wspace=0.02)
+    # plt.show()
